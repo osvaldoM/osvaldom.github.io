27fdbc142cff0b12bd5a0a7c8371af22dce76abc
osvaldom <mekymaria@gmail.com>
2021-02-08 14:39:59 +0200

    Add basic styles to blog page

--- src/pages/Blog.vue
+++ src/pages/Blog.vue
@@ -1,15 +1,23 @@
 <template>
-    <Layout>
-        <h1>Blog</h1>
-        <p>This is where I drop my random thoughts derived from my everyday experiences</p>
+  <Layout>
+    <h1 class="text-5xl">Blog</h1>
 
-        <div class="content">
-                <div v-for="edge in $page.articles.edges" :key="edge.node.id">
-                    <h2><g-link :to="`/blog/articles/${edge.node.id}`"> {{ edge.node.title }} </g-link></h2>
-                </div>
-        </div>
+    <p class="">Thoughts derived from my everyday experiences</p>
 
-    </Layout>
+    <div class="content mt-5">
+      <ul v-for="edge in $page.articles.edges" :key="edge.node.id">
+        <li class="mb-5">
+          <div class="flex items-center">
+            <span class="text-black mr-5"> March 2020</span> <span class="text-purple-600"> mysql</span>
+          </div>
+          <h2 class="text-4xl">
+            <g-link class="underline" :to="`/blog/articles/${edge.node.id}`"> {{ edge.node.title }}</g-link>
+          </h2>
+        </li>
+      </ul>
+    </div>
+
+  </Layout>
 </template>
 
 <page-query>
