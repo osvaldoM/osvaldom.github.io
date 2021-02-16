cb636ee0b14771d79b4a1356170ca6565a2bec4e
osvaldom <mekymaria@gmail.com>
2021-02-16 16:24:44 +0200

    Add left side bow and arrow

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,35 +1,37 @@
 <template>
-  <div class="bg-gradient-to-r from-rose-400 to-orange-300 py-10">
-  <div class="container mx-auto bg-white rounded-3xl px-24 py-5 shadow-2xl">
-    <header class="header flex justify-between items-center mb-6">
-      <strong>
-        <g-link to="/">
-<!--          {{ $static.metadata.siteName }}-->
-          <g-image src="~/assets/images/osvaldo.png" width="50"/>
-        </g-link>
-      </strong>
-      <nav class="nav">
-        <g-link class="nav__link font-semibold" to="/">Home</g-link>
-        <g-link class="nav__link font-semibold" to="/portfolio/">Portfolio</g-link>
-        <g-link class="nav__link font-semibold" to="/blog/">Blog</g-link>
-      </nav>
-    </header>
+  <div class="bg-gradient-to-r from-rose-400 to-orange-300 py-10 relative">
+    <div class="container mx-auto bg-white rounded-3xl px-24 py-5 shadow-2xl">
+      <header class="header flex justify-between items-center mb-6">
+        <strong>
+          <g-link to="/">
+  <!--          {{ $static.metadata.siteName }}-->
+            <g-image src="~/assets/images/osvaldo.png" width="50"/>
+          </g-link>
+        </strong>
+        <nav class="nav">
+          <g-link class="nav__link font-semibold" to="/">Home</g-link>
+          <g-link class="nav__link font-semibold" to="/portfolio/">Portfolio</g-link>
+          <g-link class="nav__link font-semibold" to="/blog/">Blog</g-link>
+        </nav>
+      </header>
 
-    <main>
-      <slot/>
-    </main>
+      <main>
+        <slot/>
+      </main>
 
-    <footer class="mt-80">
-      <ul class="home-links flex">
-        <li><a href="https://github.com/osvaldoM" target="_blank" rel="noopener">GitHub</a></li>
-        <li><a href="https://twitter.com/impurefunction" target="_blank" rel="noopener">Twitter</a></li>
-        <li><a href="https://stackoverflow.com/users/story/4043487" target="_blank" rel="noopener">Stackoverflow</a></li>
-        <li><a href="https://docs.google.com/document/d/1F8QXk3HaH0zGRcebRWAYwowFziFeIJcy7rnZuvNBXp8/edit?usp=sharing" target="_blank" rel="noopener">Resume</a></li>
-        <li class="ml-auto"><span class=""> copyright@2021</span></li>
-      </ul>
-    </footer>
-
-  </div>
+      <footer class="mt-80">
+        <ul class="home-links flex">
+          <li><a href="https://github.com/osvaldoM" target="_blank" rel="noopener">GitHub</a></li>
+          <li><a href="https://twitter.com/impurefunction" target="_blank" rel="noopener">Twitter</a></li>
+          <li><a href="https://stackoverflow.com/users/story/4043487" target="_blank" rel="noopener">Stackoverflow</a></li>
+          <li><a href="https://docs.google.com/document/d/1F8QXk3HaH0zGRcebRWAYwowFziFeIJcy7rnZuvNBXp8/edit?usp=sharing" target="_blank" rel="noopener">Resume</a></li>
+          <li class="ml-auto"><span class=""> copyright@2021</span></li>
+        </ul>
+      </footer>
+    </div>
+    <div class="striker-container default-gradient rounded-full absolute flex align-middle justify-center">
+      <img class="max-w-full text-white" svg-inline src="~/assets/svg/bow-and-arrow-2.svg" alt="responsive web app icon" width="60px" height=""/>
+    </div>
   </div>
 </template>
 
@@ -63,5 +65,12 @@ body {
 }
 .nav__link.active--exact {
   @apply rounded-2xl bg-rose-400 py-2 px-4 text-white
+}
+
+.striker-container {
+  bottom: 380px;
+  left: 200px;
+  width: 140px;
+  height: 140px;
 }
 </style>
