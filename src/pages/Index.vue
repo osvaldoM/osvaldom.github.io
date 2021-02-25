a3e09fd526efdc33f1a854a83cb77514187653b2
osvaldom <mekymaria@gmail.com>
2021-02-25 18:35:26 +0200

    Morph clip path of image

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -34,34 +34,59 @@
           <img class="max-w-full" svg-inline src="~/assets/svg/undraw_To_the_stars_qhyy.svg" alt="responsive web app icon" width="200px" height="100%" />
         </div>
         <h3 class="h3">Fast by default</h3>
-        <p>A build web applications that adapt to both desktop and mobile devices.
-          I also strive to use the latest web capabilities such as web bluetooth, offline support and more to bring the power of native apps into the web</p>
+        <p>
+            I perform web applications audits to find bottlenecks and opportunities for optimization. These audits take place at both the client/server side.
+        </p>
       </div>
       <div class="mx-3 w-1/3">
         <div class="img-container">
           <img class="max-w-full" svg-inline src="~/assets/svg/undraw_To_the_stars_qhyy.svg" alt="responsive web app icon" width="200px" height="100%"/>
         </div>
-        <h3 class="h3">Performance optimization</h3>
-        <p>I perform web applications audits to find bottlenecks and opportunities for optimization. These audits take place at both the client/server side.</p>
+        <h3 class="h3">Exciting challenges</h3>
+        <p>I'm always open to build cool things with cool people.</p>
       </div>
 
     </div>
 
 
-    <svg height="0" width="0" viewBox="0 0 16 16">
+    <svg height="0" width="0">
       <defs>
-        <clipPath clipPathUnits="objectBoundingBox" id="svgPath" transform="scale(0.0625, 0.0625)">
-          <path d='M0,0.8v9c0,0.4,0.3,1,0.8,1.3l6.5,4.4c0.4,0.3,1.1,0.3,1.5,0l6.5-4.4c0.4-0.3,0.8-0.9,0.8-1.3v-9C16,0.3,15.5,0,14.9,0H1.1 C0.5,0,0,0.3,0,0.8L0,0.8z'></path>
+        <clipPath clipPathUnits="objectBoundingBox" id="svgPath">
+          <path d='M0.049,0.69 C0,0.643,0.001,0.545,0.02,0.481 C0.04,0.413,0.074,0.401,0.143,0.316 C0.268,0.165,0.238,0.111,0.319,0.059 C0.41,0,0.528,0.016,0.604,0.051 C0.678,0.086,0.668,0.12,0.781,0.2 C0.898,0.284,0.933,0.264,0.972,0.325 C1,0.425,0.998,0.563,0.99,0.594 C0.976,0.644,0.961,0.701,0.914,0.737 C0.847,0.789,0.793,0.741,0.715,0.787 C0.649,0.826,0.664,0.875,0.6,0.935 C0.507,1,0.339,1,0.273,0.979 C0.211,0.911,0.274,0.792,0.205,0.733 C0.154,0.69,0.099,0.737,0.049,0.69'>
+
+          </path>
         </clipPath>
       </defs>
     </svg>
+    <svg class="svg">
+      <clipPath id="clip-path-2" clipPathUnits="objectBoundingBox">
+        <path d="M0.129,0.651 C0.102,0.567,0.06,0.57,0.036,0.499 C0,0.396,0.039,0.241,0.129,0.182 C0.217,0.124,0.277,0.208,0.434,0.166 C0.553,0.134,0.552,0.076,0.678,0.045 C0.732,0.032,0.864,0,0.949,0.077 C1,0.154,1,0.302,1,0.35 C1,0.458,0.974,0.471,0.978,0.57 C0.983,0.676,1,0.692,1,0.771 C1,0.855,0.946,0.949,0.869,0.964 C0.77,0.984,0.736,0.858,0.622,0.857 C0.482,0.856,0.438,1,0.314,1 C0.237,1,0.17,0.933,0.147,0.862 C0.12,0.78,0.16,0.744,0.129,0.651">
+        </path>
+      </clipPath>
+    </svg>
+
   </div>
 </template>
 
 <script>
+import anime from 'animejs/lib/anime.es.js';
+
 export default {
   metaInfo: {
     title: 'Home'
+  },
+  mounted() {
+    anime({
+      targets: '#svgPath path',
+      d: [
+        { value: 'M0.049,0.69 C0,0.643,0.001,0.545,0.02,0.481 C0.04,0.413,0.074,0.401,0.143,0.316 C0.268,0.165,0.238,0.111,0.319,0.059 C0.41,0,0.528,0.016,0.604,0.051 C0.678,0.086,0.668,0.12,0.781,0.2 C0.898,0.284,0.933,0.264,0.972,0.325 C1,0.425,0.998,0.563,0.99,0.594 C0.976,0.644,0.961,0.701,0.914,0.737 C0.847,0.789,0.793,0.741,0.715,0.787 C0.649,0.826,0.664,0.875,0.6,0.935 C0.507,1,0.339,1,0.273,0.979 C0.211,0.911,0.274,0.792,0.205,0.733 C0.154,0.69,0.099,0.737,0.049,0.69' },
+        { value: 'M0.129,0.651 C0.102,0.567,0.06,0.57,0.036,0.499 C0,0.396,0.039,0.241,0.129,0.182 C0.217,0.124,0.277,0.208,0.434,0.166 C0.553,0.134,0.552,0.076,0.678,0.045 C0.732,0.032,0.864,0,0.949,0.077 C1,0.154,1,0.302,1,0.35 C1,0.458,0.974,0.471,0.978,0.57 C0.983,0.676,1,0.692,1,0.771 C1,0.855,0.946,0.949,0.869,0.964 C0.77,0.984,0.736,0.858,0.622,0.857 C0.482,0.856,0.438,1,0.314,1 C0.237,1,0.17,0.933,0.147,0.862 C0.12,0.78,0.16,0.744,0.129,0.651' },
+        { value: 'M0.049,0.69 C0,0.643,0.001,0.545,0.02,0.481 C0.04,0.413,0.074,0.401,0.143,0.316 C0.268,0.165,0.238,0.111,0.319,0.059 C0.41,0,0.528,0.016,0.604,0.051 C0.678,0.086,0.668,0.12,0.781,0.2 C0.898,0.284,0.933,0.264,0.972,0.325 C1,0.425,0.998,0.563,0.99,0.594 C0.976,0.644,0.961,0.701,0.914,0.737 C0.847,0.789,0.793,0.741,0.715,0.787 C0.649,0.826,0.664,0.875,0.6,0.935 C0.507,1,0.339,1,0.273,0.979 C0.211,0.911,0.274,0.792,0.205,0.733 C0.154,0.69,0.099,0.737,0.049,0.69' }
+      ],
+      easing: 'easeOutQuad',
+      duration: 2000,
+      loop: true
+    });
   }
 }
 </script>
@@ -78,7 +103,15 @@ export default {
 
 .my-image {
   clip-path: url(#svgPath);
+  //transition-property: clip-path;
+  //transition-duration: 5s;
+  //transition-timing-function: ease-in-out;
+  //transition-delay: 3s;
 }
+
+//.my-image:hover {
+//  clip-path: url(#svgPath);
+//}
 .img-container {
   @apply mb-4;
 }
