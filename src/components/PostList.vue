c10b29629e48f5a7472fe7286e340355d216a5b1
osvaldom <mekymaria@gmail.com>
2021-03-07 01:02:10 +0200

    Move post list to standalone component

---
+++ src/components/PostList.vue
@@ -0,0 +1,45 @@
+<template>
+  <ul>
+    <li class="mb-20" v-for="edge in posts" :key="edge.node.id">
+      <article class="">
+        <h2 class="text-4xl mb-4">
+          <g-link class="underline" :to="`/blog/articles/${edge.node.id}`"> {{ edge.node.title }}</g-link>
+        </h2>
+        <p>{{edge.node.summary}} ...<g-link class="underline" :to="`/blog/articles/${edge.node.id}`">read more</g-link></p>
+        <div class="mt-4 flex items-center justify-between">
+          <ul class="">
+            <li>
+              <g-link class="tag"
+                      v-for="tag in edge.node.tags"
+                      :to="tag.path"
+                      :key="tag.id">
+                #{{ tag.title }}
+              </g-link>
+            </li>
+          </ul>
+          <div class="text-sm">
+            Published on
+            <time class=" mr-5"> {{ edge.node.published }}</time>
+          </div>
+        </div>
+      </article>
+    </li>
+  </ul>
+
+</template>
+
+<script>
+export default {
+  props: {
+    posts: {
+      type: Array,
+      default: null,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/Blog.vue
+++ src/pages/Blog.vue
@@ -21,32 +21,7 @@
     </ul>
 
     <div class="content mt-10">
-      <ul>
-        <li class="mb-20" v-for="edge in $page.articles.edges" :key="edge.node.id">
-          <article class="">
-          <h2 class="text-4xl mb-4">
-            <g-link class="underline" :to="`/blog/articles/${edge.node.id}`"> {{ edge.node.title }}</g-link>
-          </h2>
-          <p>{{edge.node.summary}} ...<g-link class="underline" :to="`/blog/articles/${edge.node.id}`">read more</g-link></p>
-          <div class="mt-4 flex items-center justify-between">
-            <ul class="">
-              <li>
-                <g-link class="tag"
-                        v-for="tag in edge.node.tags"
-                        :to="tag.path"
-                        :key="tag.id">
-                  #{{ tag.title }}
-                </g-link>
-              </li>
-            </ul>
-            <div class="text-sm">
-              Published on
-              <time class=" mr-5"> {{ edge.node.published }}</time>
-            </div>
-          </div>
-          </article>
-        </li>
-      </ul>
+      <post-list v-bind:posts="$page.articles.edges"></post-list>
     </div>
 
   </div>
@@ -88,9 +63,11 @@ query Articles ($page: Int) {
 <script>
 import { Pager } from 'gridsome'
 import Article from "../templates/Article";
+import PostList from "../components/PostList";
 
 export default {
   components: {
+    PostList,
     Article,
     Pager
   },
