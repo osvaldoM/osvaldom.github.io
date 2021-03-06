dd325bbab2d154dfad4165d8ef1159872540c5b7
osvaldom <mekymaria@gmail.com>
2021-03-07 01:10:45 +0200

    Customize page for tags

--- src/templates/Tag.vue
+++ src/templates/Tag.vue
@@ -1,15 +1,33 @@
 <template>
+<div>
+  <h1>Posts tagged: {{ $page.tag.title }}</h1>
 
-  <article>
-    <h1>Posts tagged {{ $page.tag.title }}</h1>
+  <hr class="my-5"/>
 
-    <ul>
-      <li v-for="post in $page.tag.belongsTo.edges" :key="post.node.id" >
-        <h3><g-link :to="post.node.path">{{ post.node.title }}</g-link></h3>
-        {{ post.node.date }}
-      </li>
-    </ul>
-  </article>
+  <ul class="tags flex mt-10 items-center">
+    <span class="mr-4">Tags:</span>
+    <li class="">
+      <g-link to="/Blog" class="tag py-4">
+        All articles
+      </g-link>
+    </li>
+    <li class="" v-for="edge in $page.tags.edges" :key="edge.node.id">
+      <g-link :to="edge.node.path" class="tag">
+        {{ edge.node.title }}
+      </g-link>
+    </li>
+  </ul>
+
+  <div class="content mt-10">
+    <post-list v-bind:posts="$page.tag.belongsTo.edges"></post-list>
+  </div>
+</div>
+<!--    <ul>-->
+<!--      <li v-for="post in $page.tag.belongsTo.edges" :key="post.node.id" >-->
+<!--        <h3><g-link :to="post.node.path">{{ post.node.title }}</g-link></h3>-->
+<!--        {{ post.node.date }}-->
+<!--      </li>-->
+<!--    </ul>-->
 </template>
 
 <page-query>
@@ -25,9 +43,12 @@ query Tag ($id: ID!, $page: Int) {
       edges {
         node {
           ...on Article {
+            id
             title
             path
             summary
+            published (format: "MMMM D, Y")
+            updated (format: "MMMM D, Y")
             tags {
               title
             }
@@ -36,11 +57,28 @@ query Tag ($id: ID!, $page: Int) {
       }
     }
   }
+  tags: allTag{
+    edges {
+      node {
+        title
+        path
+      }
+    }
+  }
 }
 </page-query>
 
 <script>
+import PostList from "../components/PostList";
+import Article from "./Article";
+import {Pager} from "gridsome";
+
 export default {
+  components: {
+    PostList,
+    Article,
+    Pager
+  },
   metaInfo() {
     return {
       title: `Tag: ${this.$page.tag.title}`
@@ -50,5 +88,11 @@ export default {
 </script>
 
 <style scoped>
+.tag {
+  @apply rounded-full bg-gray-200 mr-2 px-4 py-1.5;
+}
 
+.tag.active--exact {
+  @apply bg-gray-800 text-white;
+}
 </style>
